210862ccbd70d60c9e8573199f100b38ad0ebb87
nicco <nicco>
2016-02-08 14:33:45 -0800

    Need to fill in statement and expressions

--- Assignment2/UserCode/User2.cpp
+++ Assignment2/UserCode/User2.cpp
@@ -12,10 +12,25 @@ public:
     ~Parser ();
     void Parse ();
 private:
-    bool Block    ();
-    bool Class    ();
-    bool Function ();
-    bool Var      ();
+    bool Block      ();
+    bool Class      ();
+    bool Function   ();
+    bool Var        ();
+    bool GroupedExpression ();
+    bool Expression ();
+    bool SpecifiedType ();
+    bool Type ();
+    bool NamedType ();
+    bool FunctionType ();
+    bool Parameter ();
+    bool Scope ();
+    bool Statement ();
+    bool FreeStatement ();
+    bool DelimitedStatement ();
+    bool If ();
+    bool Else ();
+    bool While ();
+    bool For ();
 
     bool Accept (TokenType::Enum tokenType);
     bool Expect (TokenType::Enum tokenType);
@@ -30,7 +45,6 @@ Parser::Parser (const std::vector<Token> & tokenStream) :
     m_streamCursor(0)
 {}
 
-
 //=========================================================
 Parser::~Parser ()
 {}
@@ -42,36 +56,250 @@ void Parser::Parse () {
 
 //=========================================================
 bool Parser::Accept (TokenType::Enum tokenType) {
-    return false;
+    if (m_streamCursor >= (int)m_tokenStream.size()) {
+        return false;
+    }
+    Token token = m_tokenStream[m_streamCursor];
+    bool match = token.mEnumTokenType == tokenType;
+    if (match) {
+        PrintRule::AcceptedToken(token);
+        ++m_streamCursor;
+    }
+    return match;
 }
 
 //=========================================================
 bool Parser::Expect (TokenType::Enum tokenType) {
-    return false;
+    if (!Accept(tokenType)) {
+        throw ParsingException("failed parsing");
+    }
+    return true;
 }
 
 //=========================================================
 bool Parser::Block () {
     PrintRule rule("Block");
 
-    while (Class() || Function() || Var() && Expect(TokenType::Semicolon));
+    while (
+        Class() ||
+        Function() ||
+        (Var() && Expect(TokenType::Semicolon))
+    );
 
     return rule.Accept();
 }
 
 //=========================================================
 bool Parser::Class () {
-    return false;
+    PrintRule rule("Class");
+    if (Accept(TokenType::Class) == false) {
+        return false;
+    }
+
+    Expect(TokenType::Identifier);
+    Expect(TokenType::OpenCurley);
+
+    while (
+        (Var() && Expect(TokenType::Semicolon)) ||
+        Function()
+    );
+
+    return rule.Accept(Expect(TokenType::CloseCurley));
 }
 
 //=========================================================
 bool Parser::Function () {
-    return false;
+    PrintRule rule("Function");
+    if (Accept(TokenType::Function) == false) {
+        return false;
+    }
+    Expect(TokenType::Identifier);
+    Expect(TokenType::OpenParentheses);
+
+    if (Parameter()) {
+        while (Accept(TokenType::Comma)) {
+            if (!Parameter()) {
+                throw ParsingException("Expected parameter after comma in function sig");
+            }
+        }
+    }
+
+    Expect(TokenType::CloseParentheses);
+
+    SpecifiedType();
+
+    if (!Scope()) {
+        throw ParsingException("Expected scope after function");
+    }
+
+    return rule.Accept();
 }
 
 //=========================================================
 bool Parser::Var () {
+    PrintRule rule("Var");
+
+    if (Accept(TokenType::Var) == false) {
+        return false;
+    }
+
+    Expect(TokenType::Identifier);
+    if (!SpecifiedType()) {
+        throw ParsingException("No specified type provided on variable");
+    }
+
+    if (Accept(TokenType::Assignment)) {
+        if (!Expression()) {
+            throw ParsingException(
+                "expected expression on right side of var assignment");
+        }
+    }
+
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::SpecifiedType () {
+    PrintRule rule("SpecifiedType");
+    if (Accept(TokenType::Colon) == false) {
+        return false;
+    }
+    if (!Type()) {
+        throw ParsingException("Expected type in SpecifiedType");
+    }
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::Type () {
+    PrintRule rule("Type");
+    return rule.Accept(NamedType()) || rule.Accept(FunctionType());
+}
+
+//=========================================================
+bool Parser::NamedType () {
+    PrintRule rule("NamedType");
+    if (Accept(TokenType::Identifier) == false) {
+        return false;
+    }
+
+    while (Accept(TokenType::Asterisk));
+
+    Accept(TokenType::Ampersand);
+
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::FunctionType () {
+    PrintRule rule("FunctionType");
+    if (Accept(TokenType::Function) == false) {
+        return false;
+    }
+
+    Expect(TokenType::Identifier);
+    Expect(TokenType::OpenParentheses);
+
+    if (Parameter()) {
+        while (Accept(TokenType::Comma)) {
+            if (!Parameter()) {
+                throw ParsingException(
+                    "Expected Parameter after comma in function type");
+            }
+        }
+    }
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::Parameter () {
+    PrintRule rule ("Parameter");
+    return rule.Accept(
+        Accept(TokenType::Identifier) &&
+        SpecifiedType()
+    );
+}
+
+//=========================================================
+bool Parser::Scope () {
+    PrintRule rule("Scope");
+    if (Accept(TokenType::OpenCurley) == false) {
+        return false;
+    }
+    Statement();
+
+    return rule.Accept(Expect(TokenType::CloseCurley));
+}
+
+//=========================================================
+bool Parser::Statement () {
+    PrintRule rule("Statement");
+
     return false;
+}
+
+//=========================================================
+bool Parser::FreeStatement () {
+    return false;
+}
+
+//=========================================================
+bool Parser::DelimitedStatement () {
+    return false;
+}
+
+//=========================================================
+bool Parser::If () {
+    PrintRule rule("If");
+    if (Accept(TokenType::If) == false) {
+        return false;
+    }
+
+    if (!GroupedExpression()) {
+        throw ParsingException("expected group expression for if");
+    }
+    if (!Scope()) {
+        throw ParsingException("expected scope for if block");
+    }
+    Else ();
+
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::Else () {
+    PrintRule rule("Else");
+    return false;
+}
+
+//=========================================================
+bool Parser::While () {
+    PrintRule rule("While");
+    return false;
+}
+
+//=========================================================
+bool Parser::For () {
+    PrintRule rule("For");
+    return false;
+}
+
+//=========================================================
+bool Parser::Expression () {
+    PrintRule rule("Expression");
+    return false;
+}
+
+//=========================================================
+bool Parser::GroupedExpression () {
+    PrintRule rule("GroupedExpression");
+    if (Accept(TokenType::OpenParentheses) == false) {
+        return false;
+    }
+
+    Expression();
+
+    return rule.Accept(Expect(TokenType::CloseParentheses));
 }
 
 //=========================================================
@@ -85,11 +313,12 @@ void RemoveWhitespaceAndComments(std::vector<Token>& tokens) {
             tokens.begin(),
             tokens.end(),
             [](Token t) {
-                return t.mEnumTokenType == TokenType::Whitespace
-                    || t.mEnumTokenType == TokenType::SingleLineComment
-                    || t.mEnumTokenType == TokenType::MultiLineComment;
+                return (t.mEnumTokenType == TokenType::Whitespace)
+                    || (t.mEnumTokenType == TokenType::SingleLineComment)
+                    || (t.mEnumTokenType == TokenType::MultiLineComment);
             }
-        )
+        ),
+        tokens.end()
     );
 }
 
