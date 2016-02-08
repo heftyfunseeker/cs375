ab6683b5c7e0d3c55893d7cd9f3b29f5b64bafc8
Nicco Simone <niccosimone@gmail.com>
2016-02-07 22:16:26 -0800

    test 1 finished

--- Assignment2/UserCode/User2.cpp
+++ Assignment2/UserCode/User2.cpp
@@ -1,14 +1,100 @@
 /******************************************************************\
- * Author: 
+ * Author: Nicco Simone
  * Copyright 2015, DigiPen Institute of Technology
 \******************************************************************/
+
+#include <algorithm>
 #include "../Drivers/Driver2.hpp"
 
-void RemoveWhitespaceAndComments(std::vector<Token>& tokens)
-{
+class Parser {
+public:
+    Parser  (const std::vector<Token> & tokenStream);
+    ~Parser ();
+    void Parse ();
+private:
+    bool Block    ();
+    bool Class    ();
+    bool Function ();
+    bool Var      ();
+
+    bool Accept (TokenType::Enum tokenType);
+    bool Expect (TokenType::Enum tokenType);
+
+    const std::vector<Token> & m_tokenStream;
+    int m_streamCursor;
+};
+
+//=========================================================
+Parser::Parser (const std::vector<Token> & tokenStream) :
+    m_tokenStream(tokenStream),
+    m_streamCursor(0)
+{}
+
+
+//=========================================================
+Parser::~Parser ()
+{}
+
+//=========================================================
+void Parser::Parse () {
+    Block ();
 }
 
-void Recognize(std::vector<Token>& tokens)
-{
-  throw ParsingException();
+//=========================================================
+bool Parser::Accept (TokenType::Enum tokenType) {
+    return false;
+}
+
+//=========================================================
+bool Parser::Expect (TokenType::Enum tokenType) {
+    return false;
+}
+
+//=========================================================
+bool Parser::Block () {
+    PrintRule rule("Block");
+
+    while (Class() || Function() || Var() && Expect(TokenType::Semicolon));
+
+    return rule.Accept();
+}
+
+//=========================================================
+bool Parser::Class () {
+    return false;
+}
+
+//=========================================================
+bool Parser::Function () {
+    return false;
+}
+
+//=========================================================
+bool Parser::Var () {
+    return false;
+}
+
+//=========================================================
+void RemoveWhitespaceAndComments(std::vector<Token>& tokens) {
+    if (tokens.size() == 0) {
+        return;
+    }
+
+    tokens.erase(
+        std::remove_if(
+            tokens.begin(),
+            tokens.end(),
+            [](Token t) {
+                return t.mEnumTokenType == TokenType::Whitespace
+                    || t.mEnumTokenType == TokenType::SingleLineComment
+                    || t.mEnumTokenType == TokenType::MultiLineComment;
+            }
+        )
+    );
+}
+
+//=========================================================
+void Recognize (std::vector<Token>& tokens) {
+    Parser parser(tokens);
+    parser.Parse();
 }
